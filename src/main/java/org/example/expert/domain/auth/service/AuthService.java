e6fdc4b198c3e8acd54917c4a679c935a8f72ab4
tmdcksdl <tmdcksdl99@naver.com>
2025-01-03 14:16:16 +0900

    refactor: 레벨 3-3 추가 수정
    - 불필요한 주석 추가 제거

--- src/main/java/org/example/expert/domain/auth/service/AuthService.java
+++ src/main/java/org/example/expert/domain/auth/service/AuthService.java
@@ -51,7 +51,6 @@ public class AuthService {
         User user = userRepository.findByEmail(signinRequest.getEmail()).orElseThrow(
                 () -> new InvalidRequestException("가입되지 않은 유저입니다."));
 
-        // 로그인 시 이메일과 비밀번호가 일치하지 않을 경우 401을 반환합니다.
         if (!passwordEncoder.matches(signinRequest.getPassword(), user.getPassword())) {
             throw new AuthException("잘못된 비밀번호입니다.");
         }
