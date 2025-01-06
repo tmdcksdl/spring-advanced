e8288bbc21574c335ce166f5040cdd76dbd07c56
tmdcksdl <tmdcksdl99@naver.com>
2025-01-06 09:43:48 +0900

    refactor: 레벨 6-1 완료
    - 테스트 코드 실행 환경 설정(application-test.yml)
    - 비밀번호 검사 성공 테스트 코드 수정

--- src/test/java/org/example/expert/config/PasswordEncoderTest.java
+++ src/test/java/org/example/expert/config/PasswordEncoderTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+;import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(SpringExtension.class)
@@ -14,15 +15,23 @@ class PasswordEncoderTest {
     private PasswordEncoder passwordEncoder;
 
     @Test
-    void matches_메서드가_정상적으로_동작한다() {
+    void 비밀번호_검사_성공() {
         // given
         String rawPassword = "testPassword";
         String encodedPassword = passwordEncoder.encode(rawPassword);
 
         // when
-        boolean matches = passwordEncoder.matches(encodedPassword, rawPassword);
+        // matches 메서드의 매개변수 순서는 (원본 비밀번호, 인코딩된 비밀번호)이다.
+        // 따라서 매개변수의 순서가 바뀌어야 한다.
+        boolean actualResult = passwordEncoder.matches(rawPassword, encodedPassword);
 
         // then
-        assertTrue(matches);
+        // 방법 1. isEqualTo 메서드를 통해 값 비교하기
+        boolean expectedResult = true;
+        assertThat(actualResult).isEqualTo(expectedResult);
+
+        // 방법 2. assertTrue 메서드를 통해 true인지 확인하기
+        // assertTrue(actualResult);
+
     }
 }
